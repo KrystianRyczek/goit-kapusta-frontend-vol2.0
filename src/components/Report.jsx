7c37e4b7e8200a5b8e4646653b166a53647c8eb4
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-12-29 15:05:48 +0100

    Update

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,20 +1,35 @@
+import React, { useState } from 'react';
 import '../css/Report.css';
 import '../css/Transaction.css';
 import { Chart } from './Chart/Chart';
 import ReportDataSelection from './ReportDateSelection';
 import ReportsIconSet from './ReportsIconSet/ReportsIconSet';
 import ReportsIncExpSum from './ReportsIncExpSum';
+
 export default function Report({ activeSheet, expensesClass, incomesClass }) {
+  const [selectCategory, setSelectCategory] = useState(null);
+
+  const handleCategoryChange = (category) => {
+    setSelectCategory(category);
+  };
+
   return (
     <>
       <ReportDataSelection />
       <ReportsIncExpSum activeSheet={activeSheet} />
+      
       <ReportsIconSet
         activeSheet={activeSheet}
         expensesClass={expensesClass}
         incomesClass={incomesClass}
+        selectCategory={selectCategory}
+        onCategoryChange={handleCategoryChange} 
       />
-      <Chart activeSheet={activeSheet} />
+      
+      <Chart
+        activeSheet={activeSheet}
+        selectedCategory={selectCategory} 
+      />
     </>
   );
 }
