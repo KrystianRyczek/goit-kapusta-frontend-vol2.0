1a18a0848351c8f9b257fde1b308a684a54fef40
KrystianRyczek <153994194+KrystianRyczek@users.noreply.github.com>
2024-12-29 21:01:04 +0100

    Update Balance.jsx

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -8,7 +8,8 @@ import { setBalance } from "../redux/storeSlice";
 import { updateBalance } from "../redux/transaction/operation";
 
 export default function Balance() {
-  const { balanceShema, balance, setBalance } = useBalance();
+  //const { balanceShema, balance, setBalance } = useBalance();
+  const { balanceShema } = useBalance();
   const [isWelcomeModalOpen, setWelcomeModalOpen] = useState(false);
   const dispatch = useDispatch();
   const balance = useSelector((state) => state.store.balance);
