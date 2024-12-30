214618a342028a0543d61875f7649f115b11cf17
KrystianRyczek <153994194+KrystianRyczek@users.noreply.github.com>
2024-12-30 13:20:46 +0100

    fix
    fix

--- src/components/TransactionSummary.jsx
+++ src/components/TransactionSummary.jsx
@@ -2,27 +2,28 @@ import "../css/TransactionSummary.css";
 import { useTransactionSummary } from "../hooks/useTransactionSummary";
 import { v4 as uuidv4 } from "uuid";
 
-const summaryTableRow = (row) => {
-  const key = Object.keys(row);
+const summaryTableRow = (key, samaryData) => {
   return (
     <div key={uuidv4()} className="summary-table-row">
-      <p>{row[key[0]]}</p>
-      <p>{row[key[1]]} PLN</p>
+      <p key={uuidv4()}>{key}</p>
+      <p key={uuidv4()}>{samaryData[key]} PLN</p>
     </div>
   );
 };
 
 export default function TransactionSummary({ activeSheet }) {
-  const { summaryTableData } = useTransactionSummary();
-  const samaryData = summaryTableData(activeSheet);
-  // console.log("samaryData", samaryData);
+  const { data, keys } = useTransactionSummary();
+
+  const samaryData = data(activeSheet);
+  const samaryKeys = keys(samaryData);
+  
   return (
     <div className="transaction-summary-box">
       <div className="summary-table-head">
         <p className="summary-table-head-p">SUMMARY</p>
       </div>
-      {samaryData.map((item) => {
-        return summaryTableRow(item);
+      {samaryKeys.map((key) => {
+        return summaryTableRow(key, samaryData);
       })}
     </div>
   );
