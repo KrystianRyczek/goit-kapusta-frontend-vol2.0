c2ced40e90467891658232d10ad879a6952a57b0
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-12-30 14:21:42 +0100

    Logo Kapusta klik to Home '/'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,57 @@
-import '../css/NavBar.css'
-import kapustaLogo from '../images/kapusta-logo.png'
-import {useNavBar} from '../hooks/useNavBar'
-import { ConfCloseModal } from './Modal'
-
+import '../css/NavBar.css';
+import kapustaLogo from '../images/kapusta-logo.png';
+import { useNavBar } from '../hooks/useNavBar';
+import { ConfCloseModal } from './Modal';
+import { useHome } from '../hooks/useHome';
 
 export default function NavBar() {
-  
-  const { isLogin, avatar, userName, modalIsOpen, signOut, signOutConf, modalIsClose } = useNavBar()
+  const {
+    isLogin,
+    avatar,
+    userName,
+    modalIsOpen,
+    signOut,
+    signOutConf,
+    modalIsClose,
+  } = useNavBar();
 
-  if(!isLogin){
+  const { navigate } = useHome();
+
+  if (!isLogin) {
     return (
       <div className="nav-bar-container">
-          <img className="nav-bar-logo" src={kapustaLogo}  alt="Kapusta Logo"/>
+        <img className="nav-bar-logo" src={kapustaLogo} alt="Kapusta Logo" />
       </div>
     );
   }
 
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <div className="nav-bar-container">
-        <img className="nav-bar-logo" src={kapustaLogo}  alt="Kapusta Logo"/>
-        <div className="nav-bar-box">
-          <div className="nav-bar-avatar" style={{backgroundColor: `${avatar}`}}> </div>
-          <h3 className="nav-bar-user-name">Welcome {userName}! </h3>
-          <div className="nav-bar-line"></div>
-          <button className="nav-bar-button" onClick={signOut}>Exit</button>
+      <button className="nav-bar-logo-home" onClick={handleLogoClick}>
+        <img className="nav-bar-logo" src={kapustaLogo} alt="Kapusta Logo" />
+      </button>
+      <div className="nav-bar-box">
+        <div
+          className="nav-bar-avatar"
+          style={{ backgroundColor: `${avatar}` }}
+        >
+          {' '}
         </div>
-        <ConfCloseModal
-          modalIsOpen={modalIsOpen}
-          modalIsClose={modalIsClose}
-          signOutConf={signOutConf}
-
-        />
+        <h3 className="nav-bar-user-name">Welcome {userName}! </h3>
+        <div className="nav-bar-line"></div>
+        <button className="nav-bar-button" onClick={signOut}>
+          Exit
+        </button>
+      </div>
+      <ConfCloseModal
+        modalIsOpen={modalIsOpen}
+        modalIsClose={modalIsClose}
+        signOutConf={signOutConf}
+      />
     </div>
   );
 }
