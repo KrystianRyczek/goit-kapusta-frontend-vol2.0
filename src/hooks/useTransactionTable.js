bc55bcff9b3fff874e2bc0b6b2bfe6f2930532c1
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-12-30 17:54:44 +0100

    draft

--- src/hooks/useTransactionTable.js
+++ src/hooks/useTransactionTable.js
@@ -1,33 +1,55 @@
-import { useDispatch, useSelector } from "react-redux"
-import { selectIncomes, selectExpenses, selectToken } from '../redux/storeSlice';
-import { useEffect, useState } from "react";
-import { deleteUserExpense, getUserIncome, getUserExpense } from "../redux/transaction/operation";
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectIncomes,
+  selectExpenses,
+  selectToken,
+} from '../redux/storeSlice';
+import { useEffect, useState } from 'react';
+import {
+  deleteUserExpense,
+  getUserIncome,
+  getUserExpense,
+  updateBalance,
+} from '../redux/transaction/operation';
+import { useBalance } from './useBalance';
 
 export const useTransactionTable = (activeSheet) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const incomes = useSelector(selectIncomes);
   const expenses = useSelector(selectExpenses);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [transDesc, setTransDesc] = useState(null);
   const [activeData, setActiveData] = useState([]);
-  const token = useSelector(selectToken)
-  const [submit, setSubmit] = useState(false)
-  
-  const newData =  Object.values(activeData);
-  const transactionData =  newData.reduce(
-    (accumulator, currentValue) =>{ 
-      currentValue = {...currentValue, date:parseFloat(currentValue.date.slice(0, 10).replace("-", "").replace("-", ""))}
-      return [...accumulator, currentValue]}
-    , []);
-  const transactionDataSort = transactionData.sort((b, a) => new Date(a.date) - new Date(b.date))
- 
-  const tableDataSort =  transactionDataSort.reduce(
-    (accumulator, currentValue) =>{
-      currentValue = {...currentValue, date:`${currentValue.date.toString().slice(0, 4)}-`+
-                                            `${currentValue.date.toString().slice(4, 6)}-`+
-                                            `${currentValue.date.toString().slice(6, 8)}`}
-      return [...accumulator, currentValue]}
-    , []);
+  const token = useSelector(selectToken);
+  const [submit, setSubmit] = useState(false);
+  const balance = useBalance();
+  const newData = Object.values(activeData);
+  const transactionData = newData.reduce((accumulator, currentValue) => {
+    currentValue = {
+      ...currentValue,
+      date: parseFloat(
+        currentValue.date.slice(0, 10).replace('-', '').replace('-', '')
+      ),
+    };
+    return [...accumulator, currentValue];
+  }, []);
+  const transactionDataSort = transactionData.sort(
+    (b, a) => new Date(a.date) - new Date(b.date)
+  );
+
+  const tableDataSort = transactionDataSort.reduce(
+    (accumulator, currentValue) => {
+      currentValue = {
+        ...currentValue,
+        date:
+          `${currentValue.date.toString().slice(0, 4)}-` +
+          `${currentValue.date.toString().slice(4, 6)}-` +
+          `${currentValue.date.toString().slice(6, 8)}`,
+      };
+      return [...accumulator, currentValue];
+    },
+    []
+  );
 
   const transactionTableData = () => tableDataSort;
 
@@ -38,8 +60,10 @@ export const useTransactionTable = (activeSheet) => {
 
   const deleteConf = () => {
     setModalIsOpen(false);
-    setSubmit(true)
-    dispatch(deleteUserExpense({ transDesc, token }))
+    setSubmit(true);
+    dispatch(deleteUserExpense({ transDesc, token })).then(() => {
+      dispatch(updateBalance(balance)); // Aktualizacja balance po usunięciu
+    });
   };
 
   const deleteModalClose = () => {
@@ -47,7 +71,7 @@ export const useTransactionTable = (activeSheet) => {
   };
 
   useEffect(() => {
-    if (activeSheet === "expenses") {
+    if (activeSheet === 'expenses') {
       setActiveData(expenses);
     } else {
       setActiveData(incomes);
@@ -55,13 +79,12 @@ export const useTransactionTable = (activeSheet) => {
   }, [expenses, incomes, activeSheet]);
 
   const useEffectGetTransaction = () => {
-      useEffect(() => {
-        setSubmit(false)
-        dispatch(getUserIncome(token));
-        dispatch(getUserExpense(token));
-
-      }, [submit]);
-    }
+    useEffect(() => {
+      setSubmit(false);
+      dispatch(getUserIncome(token));
+      dispatch(getUserExpense(token));
+    }, [submit]);
+  };
   return {
     transactionTableData,
     useEffectGetTransaction,
--- src/redux/storeSlice.js
+++ src/redux/storeSlice.js
@@ -1,10 +1,10 @@
-import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import {
   addUser,
   signInUser,
   signOutUser,
   refreshUserToken,
-} from "./auth/operation";
+} from './auth/operation';
 import {
   addUserIncome,
   getUserIncome,
@@ -15,25 +15,25 @@ import {
   userExpenseCategory,
   userTransactionPeriodDate,
   updateBalance,
-} from "./transaction/operation";
-import { userDetails, setUserBalance } from "./user/operation";
-import { initialState } from "./initialState";
+} from './transaction/operation';
+import { userDetails, setUserBalance } from './user/operation';
+import { initialState } from './initialState';
 
 const handlePending = (state) => {
-  console.log("Pending");
+  // console.log("Pending");
   state.isLoading = true;
   state.isError = null;
   state.isRegister = false;
 };
 const handleRejected = (state, action) => {
-  console.log("Fail");
+  console.log('Fail');
   state.isLoading = false;
   state.isError = action.error.message;
   console.log(state.isError);
 };
 
 const storeSlice = createSlice({
-  name: "store",
+  name: 'store',
   initialState: initialState,
   reducers: {
     //1.readDataFromLocalStorage
@@ -81,11 +81,10 @@ const storeSlice = createSlice({
         state.incomes = [];
         state.expenses = [];
         action.payload.userData.transactions.forEach((transaction) => {
-
-          if (transaction.typeOfTransaction.toLowerCase().includes("expense")) {
+          if (transaction.typeOfTransaction.toLowerCase().includes('expense')) {
             state.expenses = [...state.expenses, transaction];
           } else if (
-            transaction.typeOfTransaction.toLowerCase().includes("income")
+            transaction.typeOfTransaction.toLowerCase().includes('income')
           ) {
             state.incomes = [...state.incomes, transaction];
           }
@@ -134,7 +133,7 @@ const storeSlice = createSlice({
         state.isLoading = false;
         state.isError = null;
         state.incomes = action.payload.incomes;
-        state.incomesStat = action.payload.monthStats
+        state.incomesStat = action.payload.monthStats;
       })
       //7.addUserExpense
       .addCase(addUserExpense.fulfilled, (state, action) => {
